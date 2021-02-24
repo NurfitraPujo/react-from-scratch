f71639cd21d8edefbd351fcd6d533b5e2a11c2c0
Nurfitra Pujo Santiko <nurfitrapujo.19@gmail.com>
2021-02-24 09:48:20 +0700

    added futher configurations and optimizations

--- webpack.config.js
+++ webpack.config.js
@@ -3,106 +3,203 @@ const HTMLwebpackplugin = require('html-webpack-plugin');
 const TSConfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
+const TerserWebpackPlugin = require('terser-webpack-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const webpack = require('webpack');
 
-module.exports = {
-  mode: 'development',
-  devtool: 'inline-source-maps',
-  resolve: {
-    plugins: [new TSConfigPathsPlugin()],
-    extensions: ['.tsx', '.ts', '.js'],
-  },
-  devServer: {
-    contentBase: path.join(__dirname, 'dist'),
-    compress: true,
-    port: 3000,
-    hot: true,
-    historyApiFallback: true,
-  },
-  entry: path.resolve(__dirname, 'src', 'index.tsx'),
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(jsx|js)$/,
-        include: path.resolve(__dirname, 'src'),
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: 'babel-loader',
-            options: {
-              presets: [
-                [
-                  '@babel/preset-env',
-                  {
-                    targets: 'defaults',
-                  },
+module.exports = function (_env, arg) {
+  const isProduction = argv.mode === 'production';
+  const isDevelopment = !isProduction;
+  return {
+    devtool: isDevelopment && 'cheap-module-source-map',
+    resolve: {
+      plugins: [new TSConfigPathsPlugin()],
+      extensions: ['.tsx', '.ts', '.js'],
+    },
+    devServer: {
+      contentBase: path.join(__dirname, 'dist'),
+      compress: true,
+      port: 3000,
+      hot: true,
+      historyApiFallback: true,
+      overlay: true,
+    },
+    entry: path.resolve(__dirname, 'src', 'index.tsx'),
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'assets/js/[name].[contenthash:8].js',
+      publicPath: '/',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(jsx|js)$/,
+          include: path.resolve(__dirname, 'src'),
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: 'babel-loader',
+              options: {
+                cacheDirectory: true,
+                cacheCompression: false,
+                envName: isProduction ? 'production' : 'development',
+                presets: [
+                  [
+                    '@babel/preset-env',
+                    {
+                      targets: 'defaults',
+                    },
+                  ],
+                  '@babel/preset-react',
                 ],
-                '@babel/preset-react',
-              ],
+              },
             },
-          },
-        ],
-      },
-      {
-        test: /\.ts(x?)$/,
-        exclude: /node_modules/,
-        use: [
-          { loader: 'ts-loader' },
-          {
-            loader: 'eslint-loader',
+          ],
+        },
+        {
+          test: /\.ts(x?)$/,
+          exclude: /node_modules/,
+          use: [
+            { loader: 'ts-loader' },
+            {
+              loader: 'eslint-loader',
+              options: {
+                fix: true,
+              },
+            },
+          ],
+        },
+        {
+          test: /\.css$/,
+          include: path.resolve(__dirname, 'src'),
+          use: [
+            isProduction
+              ? {
+                  loader: MiniCSSExtractPlugin.loader,
+                  options: {
+                    hmr: env.NODE_ENV === 'development',
+                  },
+                }
+              : 'style-loader',
+            {
+              loader: 'css-loader',
+              options: {
+                importLoaders: 1,
+              },
+            },
+            'postcss-loader',
+          ],
+        },
+        {
+          test: /\.module.css$/,
+          use: [
+            isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
+            {
+              loader: 'css-loader',
+              options: {
+                modules: true,
+                importLoaders: 1,
+              },
+            },
+            'postcss-loader',
+          ],
+        },
+        {
+          test: /\.(png|jpg|gif)$/i,
+          use: {
+            loader: 'url-loader',
             options: {
-              fix: true,
+              limit: 8192,
+              name: 'static/media/[name].[hash:8].[ext]',
             },
           },
-        ],
-      },
-      {
-        test: /\.css$/,
-        include: path.resolve(__dirname, 'src'),
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-            options: {
-              hmr: env.NODE_ENV === 'development',
+        },
+        {
+          test: /\.svg$/,
+          use: ['@svgr/webpack'],
+        },
+        {
+          test: /\.(eot|otf|ttf|woff|woff2)$/,
+          loader: require.resolve('file-loader'),
+          options: {
+            name: 'static/media/[name].[hash:8].[ext]',
+          },
+        },
+        {
+          enforce: 'pre',
+          test: /\.js$/,
+          loader: 'source-map-loader',
+        },
+      ],
+    },
+    watch: true,
+    plugins: [
+      new CleanWebpackPlugin(),
+      new HTMLwebpackplugin({
+        hash: true,
+        filename: './index.html',
+        template: path.resolve(__dirname, 'public/index.html'),
+        inject: true,
+      }),
+      isProduction &&
+        new MiniCSSExtractPlugin({
+          filename: 'assets/css/[name].[contenthash:8].css',
+          chunkFilename: 'assets/css/[name].[contenthash:8].chunk.css',
+        }),
+      new ForkTsCheckerWebpackPlugin({
+        async: false,
+      }),
+      new webpack.HotModuleReplacementPlugin(),
+      require('autoprefixer'),
+      new webpack.DefinePlugin({
+        'process.env.NODE_ENV': JSON.stringify(
+          isProduction ? 'production' : 'development'
+        ),
+      }),
+    ].filter(Boolean),
+    optimization: {
+      minimize: isProduction,
+      minimizer: [
+        new TerserWebpackPlugin({
+          terserOptions: {
+            compress: {
+              comparisons: false,
+            },
+            mangle: {
+              safari10: true,
+            },
+            output: {
+              comments: false,
+              ascii_only: true,
+            },
+            warnings: false,
+          },
+        }),
+        new OptimizeCssAssetsPlugin(),
+      ],
+      splitChunks: {
+        chunks: 'all',
+        minSize: 0,
+        maxInitialRequests: 20,
+        maxAsyncRequests: 20,
+        cacheGroups: {
+          vendors: {
+            test: /[\\/]node_modules[\\/]/,
+            name(module, chunks, cacheGroupKey) {
+              const packageName = module.context.match(
+                /[\\/]node_modules[\\/](.*?)([\\/]|$)/
+              )[1];
+              return `${cacheGroupKey}.${packageName.replace('@', '')}`;
             },
           },
-          { loader: 'style-loader' },
-          {
-            loader: 'css-loader',
-            options: {
-              importLoaders: 1,
-            },
+          common: {
+            minChunks: 2,
+            priority: -10,
           },
-          'postcss-loader',
-        ],
+        },
       },
-      {
-        test: /\.(png|svg|jpg|gif)$/i,
-        use: ['file-loader'],
-      },
-      {
-        enforce: 'pre',
-        test: /\.js$/,
-        loader: 'source-map-loader',
-      },
-    ],
-  },
-  watch: true,
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HTMLwebpackplugin({
-      hash: true,
-      filename: './index.html',
-      template: './public/index.html',
-    }),
-    new MiniCssExtractPlugin({
-      filename: '[name].bundle.css',
-      chunkFilename: '[id].css',
-    }),
-    new webpack.HotModuleReplacementPlugin(),
-    require('autoprefixer'),
-  ],
+      runtimeChunk: 'single',
+    },
+  };
 };
